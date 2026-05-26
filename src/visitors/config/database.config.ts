0348bd6542d6cb51c514a2f9e4768d4bbb104133
YasmimVieira <vieira937@gmail.com>
2026-05-25 23:00:29 -0300

    fix(config): adjust spacing for database password key for consistency

--- src/visitors/config/database.config.ts
+++ src/visitors/config/database.config.ts
@@ -8,7 +8,7 @@ export const typeOrmConfig = registerAs(
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT ?? '5432'),
     username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,  
+    password: process.env.DB_PASSWORD,   
     database: process.env.DB_NAME,
     synchronize: Boolean(process.env.DB_SYNC ?? false),
     ssl: {
