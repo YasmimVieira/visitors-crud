06cfdcd78cf3343f06b890b4d923f4997971f7c8
YasmimVieira <vieira937@gmail.com>
2026-05-25 21:29:19 -0300

    feat: enhance app module with TypeORM and configuration setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VisitorsModule } from './visitors/visitors.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { appConfigSchema, ConfigType } from './visitors/config/condig.typed';
+import { Visitor } from './visitors/entities/visitor.entity';
+import { typeOrmConfig } from './visitors/config/database.config';
+import { appConfig } from './visitors/config/app.config';
 
 @Module({
-  imports: [VisitorsModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<ConfigType>) => ({
+        ...configService.get('database'),
+        entities: [Visitor],
+      })
+    }),
+    ConfigModule.forRoot({
+      load: [typeOrmConfig, appConfig],
+      validationSchema: appConfigSchema,
+      validationOptions: { abortEarly: true },
+    }),
+    VisitorsModule,
+
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
