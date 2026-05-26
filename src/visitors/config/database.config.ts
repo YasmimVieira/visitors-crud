41307edacc776082844182bb2ceed0bdf91318f2
YasmimVieira <vieira937@gmail.com>
2026-05-25 22:34:22 -0300

    fix(config): update database environment variable keys for consistency

--- src/visitors/config/database.config.ts
+++ src/visitors/config/database.config.ts
@@ -7,9 +7,9 @@ export const typeOrmConfig = registerAs(
     type: 'postgres',
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT ?? '5432'),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: Boolean(process.env.DB_SYNC ?? false),
     ssl: {
       rejectUnauthorized: false,
